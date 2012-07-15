ca50df9d9de03a47ef9dda6d339f53b638b42bb4
Nissan Pow <nissan.pow@gmail.com>
2012-07-15 12:58:20 -0400

    .

--- webgl-2d.js
+++ webgl-2d.js
@@ -305,14 +305,14 @@
 
           // Default white background
           gl.clearColor(1, 1, 1, 1);
-          gl.clear(gl.COLOR_BUFFER_BIT); // | gl.DEPTH_BUFFER_BIT);
+          gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
           // Disables writing to dest-alpha
           gl.colorMask(1,1,1,0);
 
           // Depth options
-          //gl.enable(gl.DEPTH_TEST);
-          //gl.depthFunc(gl.LEQUAL);
+          gl.enable(gl.DEPTH_TEST);
+          gl.depthFunc(gl.LEQUAL);
 
           // Blending options
           gl.enable(gl.BLEND);
